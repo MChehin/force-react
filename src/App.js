4f4dc12d136090d1a1ad6b5fe36915a151a14426
maria <mariachehin1@gmail.com>
2022-04-21 15:15:05 -0300

    Primera versión NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+
+const stlyeParrafo = {
+  border: 'solid 5px brown',
+  fontsize: '10px',
+  color: 'white', 
+  backgroundColor: 'black',
+  fontFamily: 'Roboto',
+  padding: '10px'
+}
 
 function App() {
   return (
     <>
-    <h1>Hola FORCE GARAGE con React</h1>
-    <strong>Primer desafío</strong>
+    <NavBar></NavBar>
+    <ItemListContainer greeting=' ¡Gracias por tu visita!' ></ItemListContainer>
+    <h1 style={{color:'brown'}} className="text-4xl font-bold self-center">FORCE GARAGE</h1>
+    <p style={stlyeParrafo}>Con REACT</p>
     </>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import CartWidget from "./CartWidget";
+
+
+const styleNav = {
+backgroundColor: 'gray',
+color: 'white'
+}
+
+
+const NavBar = () => {
+  return (
+    <header className="text-gray-800 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <span className="ml-5 text-xl border">FORCE GARAGE</span>
+        </a>
+        <nav style={styleNav} className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <a className="mr-5"> INICIO </a>
+          <a className="mr-5"> MOTOS </a>
+          <a className="mr-5"> REPUESTOS </a>
+          <a className="mr-5"> ACCESORIOS </a>
+          <CartWidget></CartWidget>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
